beb5adc97e636551666f593b582ace3fa2dd84c1
m4w0rk <irvandta@gmail.com>
2025-07-03 18:04:53 +0700

    update(launch agent): fixing create connection flow

--- next.config.cjs
+++ next.config.cjs
@@ -2,31 +2,50 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
-    config.resolve.alias["~"] = path.resolve(__dirname, "src");
-    return config;
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/dashboard',
-        permanent: true,
-      },
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/chat",
-        destination: "/api/chat/route.js",
-      },
-      {
-        source: "/api/bot-info",
-        destination: "/api/bot-info/route.js",
-      },
-    ];
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack: (config) => {
+        config.resolve.alias["~"] = path.resolve(__dirname, "src");
+        return config;
+    },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, 
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,DELETE,PATCH,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
+            },
+        ];
+    },
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/dashboard",
+                permanent: true,
+            },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/api/chat",
+                destination: "/api/chat/route.js",
+            },
+            {
+                source: "/api/bot-info",
+                destination: "/api/bot-info/route.js",
+            },
+        ];
+    },
 };
